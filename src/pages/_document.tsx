83b89aa8714b60da773f2be946a4e03fd6a5a727
gibifyofficial <gibifyofficial.com>
2021-09-04 14:45:59 -0300

    fix: deleted crossorigin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default class document extends Document {
                 <Head>
                     <link rel="shortcut icon" href="favicon.png" type="favicon.png" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
                 </Head>
 
